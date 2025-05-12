40b32c1f808d4ad93abfca8868e4223a24ece43c
diego <diego109871@gmail.com>
2025-05-12 18:22:13 -0300

    agrega palabra algo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # main.py
 
 def saludar(nombre):
-    print(f"Hola, {nombre}!")
+    print(f"Hola,algo {nombre}!")
 
 if __name__ == "__main__":
     nombre_usuario = input("¿Cuál es tu nombre? ")
     saludar(nombre_usuario)
+
+
