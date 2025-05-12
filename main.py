adfa12953669baea436c25abda0f19f304a591ca
diegol109871 <el_diego_10@live.com.ar>
2025-05-11 21:44:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 
 def saludar(nombre):
-    print(f"Hola como estas, {nombre}!")
+    print(f"Hola como estas aceptame el pull, {nombre}!")
 
 if __name__ == "__main__":
     nombre_usuario = input("¿Cuál es tu nombre? ")
